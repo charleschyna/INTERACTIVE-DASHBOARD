9e5822a9bd3064aa901cc5a8885e22fa00e7ef21
RoachCodes <125027686+RoachCodes@users.noreply.github.com>
2024-04-04 15:43:16 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,28 @@ toggle.onclick = function () {
     navigation.classList.toggle("active");
     main.classList.toggle("active");
 }
+
+// Select the help section list item
+let helpSection = document.querySelector(".navigation li:nth-child(5)"); // Assuming it's the 5th list item
+
+// Add an event listener to the help section
+helpSection.addEventListener("click", handleHelpClick);
+
+// Function to handle click on the help section
+function handleHelpClick() {
+    // Add functionality here
+    window.location.href = "help.html"; // Redirect to a help page
+}
+
+// Select the dashboard section list item
+let dashboardSection = document.querySelector(".navigation li:nth-child(2)"); // Assuming it's the 5th list item
+
+
+// Add an event listener to the dashborad section
+dashboardSection.addEventListener("click", handleDashboardClick);
+
+// Function to handle click on the help section
+function handleDashboardClick() {
+    // Add functionality here
+    window.location.href = "index.html"; // Redirect to a help page
+}
